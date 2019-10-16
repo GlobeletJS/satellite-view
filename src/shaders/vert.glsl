a37c93ecd9d03f89fc6d2e68e08300874fefb0bb
Jeshurun Hembd <jjhembd@gmail.com>
2019-10-15 19:37:44 -0500

    Add sample screenshots to example, improve styling of vertex shader

--- src/shaders/vert.glsl
+++ src/shaders/vert.glsl
@@ -1,11 +1,9 @@
 attribute vec4 aVertexPosition;
-
 uniform vec2 uMaxRay;
 
 varying highp vec2 vRayParm;
 
 void main(void) {
-  vRayParm.x = aVertexPosition.x * uMaxRay.x;
-  vRayParm.y = aVertexPosition.y * uMaxRay.y;
+  vRayParm = uMaxRay * aVertexPosition.xy;
   gl_Position = aVertexPosition;
 }
