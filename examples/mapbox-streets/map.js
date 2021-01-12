2d5f3231b77fbb37e855027087a45fae9852b6a4
Jeshurun Hembd <jjhembd@gmail.com>
2021-01-12 13:35:21 -0600

    Update mapbox-streets example to use tile-setter

--- examples/mapbox-streets/map.js
+++ examples/mapbox-streets/map.js
@@ -1,25 +1,23 @@
 import { initFramebuffer } from "./framebuffer.js";
-import * as yawgl from 'yawgl';
-import * as vectorMap from 'vector-map';
-import * as projection from "./proj-mercator.js";
+import * as tileSetter from 'tile-setter';
 
 export function initMap(gl) {
   // Wrapper for maps. Handles projection of spherical coordinates
   const frame = initFramebuffer(gl, 1024, 1024);
 
-  return vectorMap.init({
+  return tileSetter.init({
     gl,
     framebuffer: frame.buffer,
     size: frame.size,
-    style: "mapbox://styles/mapbox/streets-v8",
+    style: "./streets-v8-noInteractive.json",
     mapboxToken: "pk.eyJ1IjoiamhlbWJkIiwiYSI6ImNqcHpueHpyZjBlMjAzeG9kNG9oNzI2NTYifQ.K7fqhk2Z2YZ8NIV94M-5nA",
+    units: "radians",
   }).promise.then(api => setup(api, frame.sampler))
     .catch(console.log);
 }
 
 function setup(api, sampler) {
   var loadStatus = 0;
-  const gl = api.gl;
 
   // Construct the maps.textures object
   const texture = {
@@ -39,12 +37,12 @@ function setup(api, sampler) {
     // Get map zoom
     let dMap = camPos[2] / radius *        // Normalize to radius = 1
       view.topEdge() * 2 / view.height() * // ray tangent per pixel
-      projection.scale(camPos);            // Scale assumes sphere radius = 1
+      api.projection.scale(camPos);
 
     let k = 1.0 / dMap;
     let zoom = Math.log2(k) - 9;
 
-    let changed = api.setCenterZoom(camPos, zoom, 'radians');
+    let changed = api.setCenterZoom(camPos, zoom);
     loadStatus = api.draw();
 
     let scale = api.getScale();
@@ -54,6 +52,7 @@ function setup(api, sampler) {
     console.log("scale, camPos = " + scale + ", " + mapPos);
 
     // Update mipmaps
+    const gl = api.gl;
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
     //gl.bindTexture(gl.TEXTURE_2D, sampler);
     //gl.generateMipmap(gl.TEXTURE_2D);
